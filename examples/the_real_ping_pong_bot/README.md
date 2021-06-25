556869f9b25b0f4d4c8ee68c9323e0ab2627ef93
chrisdewa <62080903+chrisdewa@users.noreply.github.com>
2021-06-25 11:43:56 -0500

    added ping pong bot

---
+++ examples/the_real_ping_pong_bot/README.md
@@ -0,0 +1,4 @@
+
+# The Real Ping Pong Bot
+
+Yes, you asked for this, so here it is.
---
+++ examples/the_real_ping_pong_bot/bot.py
@@ -0,0 +1,63 @@
+import asyncio
+from datetime import datetime
+
+import discord
+from discord.ext import commands
+from dpytools import Emoji
+
+bot = commands.Bot(command_prefix='p:')
+
+
+@bot.event
+async def on_ready():
+    print(f"{bot.user.name} ready!")
+
+
+@bot.command()
+async def ping(ctx):
+    """Plays ping pong with you"""
+    reactions = ['🏓', Emoji.X]
+
+    def check(payload: discord.RawReactionActionEvent):
+        return all([
+            ctx.author.id == payload.user_id,
+            ctx.channel.id == payload.channel_id,
+            payload.emoji.name in reactions
+        ])
+
+    def lat(first: datetime, last: datetime):
+        return f" latency: {round((last - first).total_seconds() * 1000, 1)}ms"
+
+    msg = await ctx.send('Wait a bit...')
+    await msg.edit(
+        content='Ping!' + lat(ctx.message.created_at, msg.created_at)
+    )
+
+    async def close():
+        await msg.clear_reactions()
+        await ctx.message.edit(delete_after=10)
+
+    for r in reactions:
+        await msg.add_reaction(r)
+    while True:
+        try:
+            payload = await bot.wait_for('raw_reaction_add', check=check, timeout=60)
+        except asyncio.TimeoutError:
+            await msg.edit(content="Timeout. It was fun playing with you", delete_after=10)
+            return await close()
+        else:
+            if payload.emoji.name == '🏓':
+                now = datetime.utcnow()
+                await msg.edit(
+                    content=('Ping!' if msg.content.startswith('Pong!') else 'Pong!')
+                )
+                await msg.edit(
+                    content=msg.content + lat(now, msg.edited_at)
+                )
+                await msg.remove_reaction('🏓', discord.Object(id=payload.user_id))
+            elif payload.emoji.name == Emoji.X:
+                await msg.edit(content="It was fun playing with you!")
+                return await close()
+
+
+bot.run('ODUxODcwODgzMzI4Mjk0OTQ0.YL-klQ.wlt12V9oG8qx5lzmYWIw3Q_BhY4')
